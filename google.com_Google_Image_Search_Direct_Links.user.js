e43f761d09c5508a2e82a4297c6e098584058227
romibi <romibi@bluewin.ch>
2018-03-13 14:46:30 +0100

    Fix Google Image Search Direct links

--- google.com_Google_Image_Search_Direct_Links.user.js
+++ google.com_Google_Image_Search_Direct_Links.user.js
@@ -2,25 +2,33 @@
 // @name         Google Image Search Direct Links
 // @namespace    https://romibi.ch/
 // @downloadURL  https://raw.githubusercontent.com/romibi/MyUserscripts/master/google.com_Google_Image_Search_Direct_Links.user.js
-// @version      0.1
+// @version      0.2
 // @description  Add Direct Links to Google Image Search back...
 // @author       romibi
-// @match        https://www.google.com/search*
+// @include      /^https?:\/\/(.*\.)?google..*\/search\?.*/
 // @require      https://code.jquery.com/jquery-3.3.1.slim.min.js
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
+    let updateLink = function() {
+        console.log("updating link");
+        let imglink = $('.irc_c[style*="transform: translate3d(0px, 0px, 0px)"] .irc_mi').attr("src");
+        console.log(imglink);
+        $(".rmbDirectImageLink").remove();
+        if(imglink!='undefined') {
+            let linkhtml = "<tr class=\"rmbDirectImageLink\"><td colspan=\"4\"><a href=\""+imglink+"\" class=\"i18192 r-iGpPao0iH2zA\" role=\"button\"><span>Image Direct Link (by Userscript)</span></a></td></tr>";
+            $(".irc_but_r > tbody:last-child").append(linkhtml);
+        }
+    };
+
     let images = $(".rg_l");
     images.each(function(i, elem) {
         $(elem).click(function() {
-            setTimeout(function() {
-                let imglink = $(".irc_mi").attr("src");
-                let linkhtml = "<tr class=\"rmbDirectImageLink\"><td colspan=\"4\"><a href=\""+imglink+"\" class=\"i18192 r-iGpPao0iH2zA\" role=\"button\"><span>Image Direct Link (by Userscript)</span></a></td></tr>";
-                $(".rmbDirectImageLink").remove();
-                $(".irc_but_r > tbody:last-child").append(linkhtml);
-            }, 500);
+            setTimeout(updateLink, 500);
         });
     });
+
+    setTimeout(updateLink, 1000);
 })();
