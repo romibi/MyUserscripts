938c069e76a3cf0e5d6bf9738a820f54334367ac
romibi <romibi@bluewin.ch>
2018-03-19 01:09:11 +0100

    Fix Google Image Direct Links for some cases

--- google.com_Google_Image_Search_Direct_Links.user.js
+++ google.com_Google_Image_Search_Direct_Links.user.js
@@ -2,7 +2,7 @@
 // @name         Google Image Search Direct Links
 // @namespace    https://romibi.ch/
 // @downloadURL  https://raw.githubusercontent.com/romibi/MyUserscripts/master/google.com_Google_Image_Search_Direct_Links.user.js
-// @version      0.2
+// @version      0.3
 // @description  Add Direct Links to Google Image Search back...
 // @author       romibi
 // @include      /^https?:\/\/(.*\.)?google..*\/search\?.*/
@@ -12,12 +12,16 @@
 
 (function() {
     'use strict';
-    let updateLink = function() {
+    let updateLink = function(elem) {
         console.log("updating link");
-        let imglink = $('.irc_c[style*="transform: translate3d(0px, 0px, 0px)"] .irc_mi').attr("src");
-        console.log(imglink);
+        if(typeof elem === 'undefined') {
+            let elemName = $('.irc_c[style*="transform: translate3d(0px, 0px, 0px)"]').attr("data-item-id");
+            elem = $('img[name=\''+elemName+'\']');
+        }
+        let elemJson = $(elem).parents('.rg_el').find('.rg_meta').html();
+        let imglink = $.parseJSON(elemJson).ou;
         $(".rmbDirectImageLink").remove();
-        if(imglink!='undefined') {
+        if(typeof imglink !== 'undefined') {
             let linkhtml = "<tr class=\"rmbDirectImageLink\"><td colspan=\"4\"><a href=\""+imglink+"\" class=\"i18192 r-iGpPao0iH2zA\" role=\"button\"><span>Image Direct Link (by Userscript)</span></a></td></tr>";
             $(".irc_but_r > tbody:last-child").append(linkhtml);
         }
@@ -26,7 +30,7 @@
     let images = $(".rg_l");
     images.each(function(i, elem) {
         $(elem).click(function() {
-            setTimeout(updateLink, 500);
+            updateLink(elem);
         });
     });
 
