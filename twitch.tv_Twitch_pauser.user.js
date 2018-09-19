7048b268b3c46bffc8d0a6a1268dfbb067fbfe25
romibi <romibi@bluewin.ch>
2018-09-19 23:37:46 +0200

    Fix Twitch Pauser with new Layout

--- twitch.tv_Twitch_pauser.user.js
+++ twitch.tv_Twitch_pauser.user.js
@@ -2,7 +2,7 @@
 // @name         Twitch pauser
 // @namespace    https://romibi.ch/
 // @downloadURL  https://raw.githubusercontent.com/romibi/MyUserscripts/master/twitch.tv_Twitch_pauser.user.js
-// @version      0.3
+// @version      0.4
 // @description  Pause the autoplaying video on Twitch.tv based on: https://webapps.stackexchange.com/a/106767
 // @author       Zach Saucier, romibi
 // @match        https://www.twitch.tv/
@@ -11,23 +11,22 @@
 
 (function() {
     'use strict';
+    var attempts = 50;
+    var playerDivSelector = ".front-page-carousel .player"
 
     var mySetInvertal = setInterval(function() {
-        //console.log("userscript pause twitch running");
-        var playerdiv = document.querySelector(".featured-broadcasters .carousel-player .player");
+        var playerdiv = document.querySelector(playerDivSelector);
         if(playerdiv !== null) {
-            //var playerIframeDoc = playerIframe.contentWindow.document;
-            //var videoElem = playerIframeDoc.querySelector("video");
             var playPause = playerdiv.querySelector(".qa-pause-play-button");
-
-            //playerdiv.addEventListener("loadeddata", clickPlayButton(playPause));
-            //var paused = playerdiv.getAttribute('data-paused');
             var playPauseLabel = playPause.querySelector(".player-tip").getAttribute("data-tip");
             var paused = playPauseLabel !== "Pause";
-            //console.log(paused);
+
             if(!paused) {
-                //console.log("click");
                 playPause.click();
+                clearInterval(mySetInvertal);
+            }
+        } else {
+            if(attempts--<0) {
                 clearInterval(mySetInvertal);
             }
         }
