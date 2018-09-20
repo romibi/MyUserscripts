c9efad01d4b0a942766e8031a5c333e7adfbd28c
romibi <romibi@bluewin.ch>
2018-09-20 02:32:01 +0200

    Simplify and Fix Gyazo Auto Direct Link

--- gyazo.com_Auto_Direct_Link.user.js
+++ gyazo.com_Auto_Direct_Link.user.js
@@ -2,7 +2,7 @@
 // @name         Gyazo Auto Direct Link
 // @namespace    http://romibi.ch/
 // @downloadURL  https://raw.githubusercontent.com/romibi/MyUserscripts/master/gyazo.com_Auto_Direct_Link.user.js
-// @version      0.1
+// @version      0.2
 // @description  automatically copy gyazo direct links to clipboard
 // @author       romibi
 // @match        https://gyazo.com/*
@@ -11,13 +11,7 @@
 
 (function() {
     'use strict';
-
-    $(document).ready(function() {
-        if(document.referrer=="") {
-            $('.shr-menu-btn')[0].click();
-            $('.share-menu-component .code-btn-box .operation-btn')[0].click();
-            GM_setClipboard($($('.share-menu-component .code-btn-box .copy-area input')[0]).attr("value"));
-            $('.shr-menu-btn')[0].click();
-        }
-    });
+    if(document.referrer=="") {
+        GM_setClipboard(document.querySelector('link[rel="image_src"]').href);
+    }
 })();
